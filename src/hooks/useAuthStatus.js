aa0fcd158210b0c57c935df234df5596ded3ace3
t-kalden <kalden.t@outlook.com>
2022-12-08 19:06:18 -0500

    custom hook to check auth status

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,24 @@
+import { useEffect, useState, useRef } from "react"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+
+export const useAuthStatus = () => {
+    const [ loggedIn, setLoggedIn ] = useState(false)
+    const [ checkingStatus, setCheckingStatus ] = useState(true)
+    const isMounted = useRef(true)
+
+    useEffect(() => {
+        if(isMounted) {
+            const auth = getAuth()
+
+            onAuthStateChanged(auth, (user) => {
+                user ? setLoggedIn(true) : setLoggedIn(false)
+                setCheckingStatus(false)
+            })
+        }
+        return () => {
+            isMounted.current = false
+        }
+    })
+
+    return { loggedIn, checkingStatus }
+}

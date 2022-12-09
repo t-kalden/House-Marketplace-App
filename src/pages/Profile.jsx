6407d5fb49464f210b6c2c5f2150eaa95ba0cd1c
t-kalden <kalden.t@outlook.com>
2022-12-09 11:28:18 -0500

    adding link to create listing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react"
 import { getAuth,  updateProfile } from "firebase/auth"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import { doc, updateDoc } from "firebase/firestore"
 import { db } from "../firebase.config"
 import { toast } from "react-toastify"
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
+
 
 function Profile() {
   const auth = getAuth()
@@ -82,6 +85,12 @@ function Profile() {
           />
         </form>
       </div>
+
+      <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt="home"/>
+          <p>Sell or Rent your home</p>
+          <img src={arrowRight} alt="arrow right"  />
+      </Link>
     </main>
   </div>
 }

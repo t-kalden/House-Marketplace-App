c46db2e6bc2714e40574b6798a58d966d6481066
t-kalden <kalden.t@outlook.com>
2022-12-15 10:39:13 -0500

    adding navigate logic to onEdit function

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -95,6 +95,8 @@ function Profile() {
     }
   }
 
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`)
+
   return <div className="profile">
     <header className="profileHeader">
       <p className="pageHeader">My Profile</p>
@@ -145,6 +147,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={()=> onDelete(listing.id)}
+                  onEdit={()=> onEdit(listing.id)}
                 />
               ))
             }

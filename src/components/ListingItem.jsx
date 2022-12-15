8d1bcfd9be1f4f6df3a8b9252ce22588e48d4de3
t-kalden <kalden.t@outlook.com>
2022-12-15 10:38:36 -0500

    adding edit icon to listing item

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom"
 import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg"
 import { ReactComponent as BedIcon } from "../assets/svg/bedIcon.svg"
 import { ReactComponent as BathtubIcon } from "../assets/svg/bathtubIcon.svg"
+import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg"
 
-function ListingItem({ listing, id, onDelete }) {
+function ListingItem({ listing, id, onDelete, onEdit }) {
   return (
     <li className="categoryListing">
         <Link to={`/category/${listing.type}/${id}`}
@@ -46,6 +47,13 @@ function ListingItem({ listing, id, onDelete }) {
                 onClick={() => onDelete(listing.id, listing.name)}
             />
         )}
+
+        { onEdit && 
+            <EditIcon 
+                className='editIcon'
+                onClick={() => onEdit(id)}
+            />
+        }
     </li>
   )
 }
